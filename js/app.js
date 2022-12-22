e9f858615d032f3ae533071c3b39405fee7088df
ahmedmeid99 <you@example.com>
2022-12-22 20:01:46 +0200

    fix-toggle-menu

--- js/app.js
+++ js/app.js
@@ -6,11 +6,18 @@ form.addEventListener('submit', (e) => e.preventDefault())
 /////////////////
 // close menu after click on menu-link
 const menu = document.querySelector('.navbar-collapse')
+const menuIcon = document.querySelector('.navbar-toggler')
 const links = document.querySelectorAll('.nav-item a')
+const logo = document.querySelector('.navbar-brand')
 links.forEach((link) => {
     link.addEventListener('click', () => {
         menu.classList.remove('show')
+        menuIcon.setAttribute('aria-expanded', "false")
     })
+})
+logo.addEventListener('click', () => {
+    menu.classList.remove('show')
+    menuIcon.setAttribute('aria-expanded', "false")
 })
 // animation
 AOS.init({
